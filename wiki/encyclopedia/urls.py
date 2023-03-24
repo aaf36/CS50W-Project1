a1b4a24f03240cc7b8520c059631b791e97eef15
Ali Faour <aaf36@mail.aub.edu>
2023-03-25 00:09:55 +0200

    implemented entry page and index page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+app_name ='wiki'
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:title>", views.entry, name="get_entry")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.urls import reverse
+
 
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
+
+def entry(request, title):
+    entries = util.list_entries()
+    for entry in entries:
+        if title.lower() == entry.lower():
+            return render(request, "encyclopedia/entry.html", {
+                "title":title,
+                "title_content": util.get_entry(title),
+            })
+    return render(request, "encyclopedia/error404.html")
 
