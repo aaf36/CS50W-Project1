682230ea1b9aad6b880c07185d5d7dcd3994a845
Ali Faour <aaf36@mail.aub.edu>
2023-03-25 22:52:16 +0200

    added correct functionality to search view as well as query substring matching

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,6 +7,6 @@ app_name ='wiki'
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:entry_title>", views.entry_view, name="get_entry"),
+    path("get/<str:entry_title>", views.entry_view, name="get_entry"),
     path("search", views.search, name="search")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -9,33 +9,53 @@ from . import util
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries(),
+        "entries": util.list_entries()
     })
+
+
+def convert_markdown_to_Html(markdownText):
+    HtmlText =markdown2.markdown(markdownText)
+    return HtmlText
+
 
 def entry_view(request, entry_title):
     entries= util.list_entries()
     for entry in entries:
         if entry_title.lower()== entry.lower():
             text =util.get_entry(entry_title)
-            entry_content= markdown2.markdown(text)
+            entry_content= convert_markdown_to_Html(text)
             return render(request, "encyclopedia/entry.html", {
                     "entry_title":entry_title,
                     "entry_content":entry_content
                 })
-            
-    return render(request, "encyclopedia/error404.html", status=404)
+    return render(request, "encyclopedia/error404.html")
+   
     
-    
+
          
 
 def search(request):
-    entries = util.list_entries()
-    query = request.GET['q']
-    for entry in entries:
-            if query.lower() == entry.lower():
-                return redirect("wiki:get_entry", entry=query)
+    matched_queries=[]
+    if request.method =='GET':
+        query =request.GET.get('q')
+        Squery =str(query)
+        entiries =util.list_entries()
+        for entry in entiries:
+            if Squery.lower()==entry.lower():
+                query_text= util.get_entry(query)
+                query_content = convert_markdown_to_Html(query_text)
+                return render(request, "encyclopedia/entry.html", {
+                    "entry_title": query,
+                    "entry_content": query_content
+                })
+            elif Squery.lower() in entry.lower():
+                matched_queries.append(entry)
+                return render(request, "encyclopedia/search_result.html", {
+                "results":matched_queries
+                })
                 
         
+
 
 
 
